8bd6179565f9f79aca7b5412bd31414388ba350f
Samuel Muigai <ndungusam01@gmail.com>
2024-01-05 16:02:48 +0300

    Created a readME file. Its content to follow

--- lib/models.py
+++ lib/models.py
@@ -13,7 +13,7 @@ class Customer(Base):
     last_name=Column(String)
 
     reviews = relationship('Review', back_populates='customers')
-    restaurants = relationship('Restaurant',secondary='reviews' ,back_populates='customers')
+    restaurants = relationship('Restaurant',secondary='reviews' ,back_populates='customers',viewonly=True)
 
     def __repr__(self):
         return f"Customer(id={self.id}, first_name={self.first_name}, last_name={self.last_name})"
